6ad5cce69a59e85bc6c1438bfd32ff6f9a16740c
HanJeongmok <wjdahe1845@gmail.com>
2026-02-24 15:12:29 +0900

    first commit

---
+++ src/main/java/com/back/jsb_exam/JsbExamApplication.java
@@ -0,0 +1,13 @@
+package com.back.jsb_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsbExamApplication.class, args);
+	}
+
+}
