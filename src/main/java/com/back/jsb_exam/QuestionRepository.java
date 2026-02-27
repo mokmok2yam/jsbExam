f4d28f12aaac9a77e7996d746c560b2c0962a519
HanJeongmok <wjdahe1845@gmail.com>
2026-02-27 12:32:54 +0900

    11강 12강 findBySubject만들어서 테스트 진행

--- src/main/java/com/back/jsb_exam/QuestionRepository.java
+++ src/main/java/com/back/jsb_exam/QuestionRepository.java
@@ -2,6 +2,9 @@ package com.back.jsb_exam;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
 
+    Optional<Question> findBySubject(String subject);
 }
--- src/test/java/com/back/jsb_exam/QuestionRepositoryTest.java
+++ src/test/java/com/back/jsb_exam/QuestionRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.back.jsb_exam;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,5 +25,16 @@ public class QuestionRepositoryTest {
 
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+    @Test
+    void t2(){
+        Question q1 = questionRepository.findById(1).get();
+        assertEquals("sbb가 무엇인가요?",q1.getSubject());
+    }
+    @Test
+    void t3(){
+        Question q1=questionRepository.findBySubject("sbb가 무엇인가요?").get();
+        Assertions.assertThat(q1.getId()).isEqualTo(1);
+        Assertions.assertThat(q1.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
     }
 }
